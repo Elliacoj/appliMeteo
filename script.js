e0476338ad2919e2335be5d2c18a7bc20af5025e
Elliacoj <73291249+Elliacoj@users.noreply.github.com>
2021-01-13 14:31:51 +0100

    Modif

--- script.js
+++ script.js
@@ -1,25 +1,7 @@
-const site = "http://api.openweathermap.org/data/2.5/weather?lat=50.0333&lon=4.1667&units=metric&lang=fr&appid=58ffdc9405cc39b5cc2687237659505b";
-
 let country = document.getElementById("country");
 let time = document.getElementById("time");
 let temperature = document.getElementById("temperature");
 let weather = document.getElementById("weather");
-let xhr = new XMLHttpRequest;
-
-xhr.open("GET", site);
-xhr.responseType = "json";
-
-
-xhr.onload = function () {
-    let response = xhr.response;
-    console.log(response);
-
-    country.innerHTML = xhr.response.name;
-    temperature.innerHTML = xhr.response.main.temp + "°";
-    weather.innerHTML = xhr.response.weather[0].description;
-}
-
-xhr.send();
 
 function timeCooldown() {
     let date = new Date;
@@ -32,3 +14,14 @@ function timeCooldown() {
 timeCooldown();
 setInterval(timeCooldown, 1000);
 
+$.ajax({
+    url: "http://api.openweathermap.org/data/2.5/weather?lat=50.0333&lon=4.1667&units=metric&lang=fr&appid=58ffdc9405cc39b5cc2687237659505b",
+    method: "GET",
+    dataType: "json"
+})
+
+    .done(function (response) {
+        country.innerHTML = response.name;
+        temperature.innerHTML = response.main.temp + "°";
+        weather.innerHTML = response.weather[0].description;
+    });
