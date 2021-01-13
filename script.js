fab4c39346844980b3c00527b8a3f8a7d2445385
Elliacoj <73291249+Elliacoj@users.noreply.github.com>
2021-01-13 15:39:49 +0100

    Modif

--- script.js
+++ script.js
@@ -1,27 +1,30 @@
-let country = document.getElementById("country");
-let time = document.getElementById("time");
-let temperature = document.getElementById("temperature");
-let weather = document.getElementById("weather");
+let country = $('#country');
+let time = $('#time');
+let temperature = $('#temperature');
+let weather = $('#weather');
+let input = $('#choice');
 
 function timeCooldown() {
     let date = new Date;
     let minutes = date.getMinutes().toString();
     let seconds = date.getSeconds().toString();
 
-    time.innerHTML = date.getHours() +":" + minutes.padStart(2, "0") + ":" + seconds.padStart(2, "0");
+    time.text(date.getHours() +":" + minutes.padStart(2, "0") + ":" + seconds.padStart(2, "0"));
 }
 
 timeCooldown();
 setInterval(timeCooldown, 1000);
 
-$.ajax({
-    url: "http://api.openweathermap.org/data/2.5/weather?lat=50.0333&lon=4.1667&units=metric&lang=fr&appid=58ffdc9405cc39b5cc2687237659505b",
-    method: "GET",
-    dataType: "json"
-})
+$('#send').click(function () {
+    $.ajax({
+        url: "http://api.openweathermap.org/data/2.5/weather?q="+ input.val() +"&units=metric&lang=fr&appid=58ffdc9405cc39b5cc2687237659505b",
+        method: "GET",
+        dataType: "json"
+    })
 
-    .done(function (response) {
-        country.innerHTML = response.name;
-        temperature.innerHTML = response.main.temp + "°";
-        weather.innerHTML = response.weather[0].description;
-    });
+        .done(function (response) {
+            country.text(response.name);
+            temperature.text(response.main.temp + "°");
+            weather.text(response.weather[0].description);
+        });
+})
